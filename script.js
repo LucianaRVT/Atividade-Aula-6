fad6d35ca31da6ff48293d11258bb13cfaf0189d
LucianaRVT <122409763+LucianaRVT@users.noreply.github.com>
2023-01-19 16:49:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var imprimir = "";
 var ano = ["Janeiro","Fevereiro","Março", "Abril", "Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro"];
 alert(ano.length);
 for(var i = 0; i < ano.length; i ++){
-    imprimir += ano [2] + "<br>";
+    imprimir += ano[i] + "<br>";
 }
 saida.innerHTML = imprimir;
